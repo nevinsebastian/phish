d2a2e8c40599187f10c463730ab239361fbd96f6
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-04-05 16:56:11 +0530

    redierct

--- Frontend/phish/src/App.js
+++ Frontend/phish/src/App.js
@@ -1,12 +1,13 @@
 // src/App.js
 import React from "react";
-import URLForm from "./components/URLForm";
+import URLChecker from "./components/URLChecker";
+
 
 function App() {
   return (
     <div className="App">
       <h1>URL Classifier</h1>
-      <URLForm />
+      <URLChecker/>
     </div>
   );
 }
