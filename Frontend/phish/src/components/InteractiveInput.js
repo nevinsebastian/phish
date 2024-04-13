5fbc2bf5406a1cc7f4a85b67e55c5455d3643fe4
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-04-13 15:03:29 +0530

    added style

--- Frontend/phish/src/components/InteractiveInput.js
+++ Frontend/phish/src/components/InteractiveInput.js
@@ -7,7 +7,7 @@ const InteractiveInput = () => {
   return (
     <div className="interactive-input" style={{ width: "100vw", height: "100vh", backgroundImage: `url(/BG.png)`, backgroundSize: "cover", backgroundPosition: "center", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <div className="search-bar-container">
-        <input type="text" className="search-bar" placeholder="Search" />
+        <input type="text" className="search-bar" placeholder="Enter your Link" />
       </div>
       <div className="plus-jakarta-sans plus-jakarta-sans-bold" style={{ position: "absolute", top: 90, left: 90 }}>PhisherX</div>
       <div className="help-text">#We are here to help to stay safe</div>
