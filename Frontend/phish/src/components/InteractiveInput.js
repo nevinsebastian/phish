2b71015e5685a9494827585c4d9dce5230a32528
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-04-13 15:35:54 +0530

    added style

--- Frontend/phish/src/components/InteractiveInput.js
+++ Frontend/phish/src/components/InteractiveInput.js
@@ -9,8 +9,8 @@ const InteractiveInput = () => {
       <div className="search-bar-container">
         <input type="text" className="search-bar" placeholder="Enter your Link" />
       </div>
-      <div className="plus-jakarta-sans plus-jakarta-sans-bold" style={{ position: "absolute", top: 90, left: 90 }}>PhisherX</div>
-      <div className="help-text">#We are here to help to stay safe</div>
+      <div className="plus-jakarta-sans plus-jakarta-sans-bold" style={{ position: "absolute", top: 90, left: 90 }}>PhisherX<span className="dot"></span></div>
+      <div className="help-text">We are here to help to stay safe</div>
       {/* Your other content here */}
     </div>
   );
